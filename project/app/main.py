92d512f92b350ffa2f817a0b757f4f2e0ea7f421
lilchoto3 <lilchoto3@gmail.com>
2020-08-25 13:29:36 -0700

    NEW: added getdata route to app

--- project/app/main.py
+++ project/app/main.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api import predict, viz
+from app.api import predict, viz, getdata
 
 app = FastAPI(
-    title='YOUR-PRODUCT-TEAM-NAME DS API',
+    title='Human Rights First Data Science API',
     description='Replace this placeholder text',
-    version='0.1',
+    version='0.2',
     docs_url='/',
 )
 
 app.include_router(predict.router)
 app.include_router(viz.router)
+app.include_router(getdata.router)
 
 app.add_middleware(
     CORSMiddleware,
