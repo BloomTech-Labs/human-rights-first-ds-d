b2c24c9b2ab58ab38323f827f6eac45142a45977
lilchoto3 <lilchoto3@gmail.com>
2020-09-21 13:07:50 -0700

    larger data pull, longer timer, output fixes

--- project/app/api/getdata.py
+++ project/app/api/getdata.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 import pandas as pd
 from .update import backlog_path
+from ast import literal_eval
 
 router = APIRouter()
 
@@ -14,7 +15,13 @@ async def getdata():
         with open(backlog_path) as f:
             pass  # file can be opened successfully
         # load backlog and return it
-        backlog = pd.read_csv(backlog_path)
+        backlog = pd.read_csv(
+            backlog_path,
+            converters={
+                'links': literal_eval,
+                'geocoding': literal_eval
+            }
+        )
         return backlog.to_json(orient='records')
     except IOError:
         # file cannot be opened successfully and needs to be created
